4cc9aa8fc562e91c75af70934b87815f6bafbaa0
timothy <tim.longoria@gmail.com>
2017-12-25 10:37:16 -0600

    Starts event handlers for buttons

---
+++ js/simon.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var bluePress = document.getElementById("blue");
+  bluePress.addEventListener("click", function () {
+    alert("clicked blue");
+  });
+
+});
+
+// Add function definitions below here
